44d8c15a7f65207474c43df246ca15729e09432a
John Polling <john@theled.co.uk>
2015-08-06 11:26:33 +0100

    Allow any media id

--- lib/groupon_api.rb
+++ lib/groupon_api.rb
@@ -4,6 +4,6 @@ require "groupon_api/deals"
 require "groupon_api/request"
 
 module GrouponApi
-  API_KEY_FORMAT = /UK_AFF_0_\d+_212556_0/
+  API_KEY_FORMAT = /UK_AFF_0_\d+_\d_0/
   API_BASE = 'partner-int-api.groupon.com'
 end
